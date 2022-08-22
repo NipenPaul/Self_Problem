ae8d11249559d892b9af763f6956fe518648f3e5
NipenPaul <53397507+NipenPaul@users.noreply.github.com>
2022-08-22 10:12:36 +0600

    Add files via upload

---
+++ myTemplete.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define int int64_t
+#define endl '\n'
+#define Faster ios::sync_with_stdio(false);cin.tie(nullptr);
+#define CHECK(x) cout << (#x) << " is " << (x) << endl;
+
+using namespace std;
+
+const int N = (int)1e5+5;
+
+void solution(int tc){
+	int n; cin >> n;
+	vector<int> v(n);
+	for(int i = 0; i < n; i++){
+        cin >> v[i];
+	}
+}
+int32_t main(){
+	Faster;
+	int tc = 1;
+	cin >> tc;
+	for(int i = 1; i <= tc; i++){
+		solution(i);
+	}
+    return 0;
+}
